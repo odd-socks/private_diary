84b8c028ef0c7e0d319b6bdf8f7a04f6430632ed
student <student@o-hara-kashiwa.local>
2021-09-06 12:14:16 +0900

    初期セットアップ（テンプレ－ト登録）

---
+++ private_diary/private_diary/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path, path
+from django.urls.conf import include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('diary.urls'))
+]
