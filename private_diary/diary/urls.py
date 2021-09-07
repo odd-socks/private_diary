86940f56c322bd9d262476c4d6754b8730cf04bb
student <student@o-hara-kashiwa.local>
2021-09-07 12:31:18 +0900

    問い合わせフォーム作成

--- private_diary/diary/urls.py
+++ private_diary/diary/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-
 from . import views 
 
 
 app_name = 'diary'
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
+    path('inquiry/', views.InquiryView.as_view(), name='inquiry')
 ]
--- private_diary/diary/views.py
+++ private_diary/diary/views.py
@@ -1,7 +1,10 @@
-from re import template
-from django.shortcuts import render
 from django.views import generic
+from .forms import InquiryForm
 
-# Create your views here.
+
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+    form_class = InquiryForm
