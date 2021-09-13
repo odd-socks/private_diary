ef78fff59e93d399bcf94e26873ffc3219c673d9
Naoki <freeadores@gmail.com>
2021-09-13 15:46:19 +0000

    CRUD実装

--- private_diary/private_diary/urls.py
+++ private_diary/private_diary/urls.py
@@ -1,10 +1,15 @@
-
 from django.contrib import admin
-from django.urls import path, path
+from django.urls import path
 from django.urls.conf import include
+from django.contrib.staticfiles.urls import static
+from .import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('diary.urls')),
-    path('accounts/', include('allauth.urls')),
+    path('accounts/' ,include('allauth.urls')),
 ]
+
+#開発サーバーでメディアを配信できるようにする設定
+urlpatterns += static(settings.MEDIA_URL,
+document_root=settings.MEDIA_ROOT)
